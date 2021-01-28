4f6e12b8e74ca933cfa4290953cf21790c2d8134
Fabrizio Herrera <javierherrera619@outlook.com>
2021-01-28 16:10:35 -0500

    modifié la page d'accueil

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const nom = 'Monde';
+
   return (
+
+
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          allo {nom}
         </p>
         <a
           className="App-link"
